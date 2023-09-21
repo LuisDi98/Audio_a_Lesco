972489ff4fc50d37571fd5bef8072759e524793b
LuisDiMora98 <aguilarluisdi@estudiantec.cr>
2023-09-21 14:59:08 -0600

    tweaks docker and reqs

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 from flask import Flask, request, render_template
-import torch
 import whisper
-import ipywidgets as widgets
-from IPython import display as disp
-from IPython.display import display, Audio, clear_output
-import base64
-from pydub import AudioSegment
-import io
-import tempfile
-import librosa
-from scipy.io import wavfile
-import numpy as np
-import soundfile as sf
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 import sounddevice as sd
 from scipy.io.wavfile import write
 
 app = Flask(__name__)
 tokenizer = None
 model = None
-
-
 
 
 def transcript(file_name):
